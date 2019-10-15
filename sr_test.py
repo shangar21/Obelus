8d39864f3da89612aa1f99d6604067fc0b2ff1fe
shangar21 <56494763+shangar21@users.noreply.github.com>
2019-10-14 23:30:00 -0400

    made adding, multiplying, subtracting and dividing functions

--- sr_test.py
+++ sr_test.py
@@ -1,6 +1,35 @@
 
 import speech_recognition as sr
 
+def int_addition(text: str) -> int:
+    index = text.find("+")
+    x = int(text[:index - 1])
+    y = int(text[index + 2:])
+    return x + y
+
+
+def int_subtraction(text: str) -> int:
+    index = text.find("-")
+    x = int(text[:index - 1])
+    y = int(text[index + 2:])
+    return x - y
+
+
+
+def int_multiply(text: str) -> int:
+    index = text.find("*")
+    x = int(text[:index - 1])
+    y = int(text[index + 2:])
+    return x * y
+
+
+def int_devide(text: str) -> int:
+    index = text.find("\\")
+    x = int(text[:index - 1])
+    y = int(text[index + 2:])
+    return (x//y)
+
+
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
@@ -8,10 +37,16 @@ with sr.Microphone() as source:
     audio = r.listen(source)
 
     try:
-       text = r.recognize_google(audio)
-       print(text)
-       index = text.find("+")
-       print(int(text[: index - 1]) + int(text[index + 2:]))
+        text = r.recognize_google(audio)
+        print(text)
+        print(int_subtraction(text))
 
     except:
         print("Sorry, unable to understand.")
+
+#print(int_multiply(text))
+
+#print(int_addition(text))
+
+#print(int_subtraction(text))
+
